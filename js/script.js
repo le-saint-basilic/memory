ebda5998b9b70689154e7132e488cd64ca034f65
Damien <damien.cosse@etu.unicaen.fr>
2026-09-18 12:11:33 +0200

    feat: dynamic DOM generation for game board with data-attributes and a11y properties

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const dimension = 150;
 const imgStart = Math.floor(Math.random()*100)+1;
 let images = [];
 
+const board = document.getElementById("game-board")
+
 function shuffle(cards){
     for (let index = cards.length-1; index >= 0; index--) {
         const i = Math.floor(Math.random()*(index+1));
@@ -15,7 +17,21 @@ for (let index = imgStart; index < (imgStart + 8); index++) {
 }
 
 let cards = [...images, ...images];
-cards = shuffle(cards);
 
 
-console.log(cards);
+function initGame(){
+    cards = shuffle(cards);
+    cards.forEach(url => {
+        const card  = document.createElement("div");
+        card.className = "cards";
+        card.dataset.value = url;
+        card.role = "button";
+        card.tabIndex='0';
+        board.appendChild(card)
+    });
+    
+}
+
+
+initGame();
+
